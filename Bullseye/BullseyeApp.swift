5339eecd4159fb4db8424da4a4e9a300a89a593f
Tianchi "Maverick" Wu <tianchiwu.27@gmail.com>
2025-08-15 13:27:40 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Tianchi "Maverick" Wu on 8/15/25.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by Tianchi "Maverick" Wu on 8/15/25.
+//
+
+import Testing
+@testable import Bullseye
+
+struct BullseyeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
