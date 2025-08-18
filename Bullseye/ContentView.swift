b60bd5988992111453e0bb9c73e72aa679c65bb6
Tianchi "Maverick" Wu <tianchiwu.27@gmail.com>
2025-08-18 14:22:04 -0400

    Create some Views

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,12 +10,27 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        .bold()
+        .multilineTextAlignment(.center)
+        .lineSpacing(4.0)
+        .font(.footnote)
+        .kerning(2.0)
+      Text("89")
+        .fontWeight(.black)
+        .font(.largeTitle)
+        .kerning(-1.0)
+      HStack {
+        Text("1")
+          .bold()
+        Slider(value: .constant(50), in: 1.0...100.0)
+        Text("100")
+          .bold()
+      }
+      Button("Hit me") {
+        
+      }
     }
-    .padding()
   }
 }
 
