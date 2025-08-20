16752e52fd287a904bd7e344f65e6c833db36471
Tianchi "Maverick" Wu <tianchiwu.27@gmail.com>
2025-08-19 20:48:44 -0400

    Update project structure

---
+++ Bullseye/Models/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by Tianchi "Maverick" Wu on 8/18/25.
+//
+
+import Foundation
+
+struct Game {
+  var target: Int = 27
+  var score: Int = 0
+  var round: Int = 1
+  
+  func points(sliderValue: Int) -> Int {
+    return 999
+  }
+}
---
+++ Bullseye/Views/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Tianchi "Maverick" Wu on 8/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  @State private var alertIsVisible: Bool = false
+  @State private var sliderValue: Double = 50.0
+  @State private var game: Game = Game()
+  
+  var body: some View {
+    VStack {
+      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        .bold()
+        .multilineTextAlignment(.center)
+        .lineSpacing(4.0)
+        .font(.footnote)
+        .kerning(2.0)
+      Text(String(game.target))
+        .fontWeight(.black)
+        .font(.largeTitle)
+        .kerning(-1.0)
+      HStack {
+        Text("1")
+          .bold()
+        Slider(value: $sliderValue, in: 1.0...100.0)
+        Text("100")
+          .bold()
+      }
+      Button("Hit me") {
+        alertIsVisible = true
+      }
+      .alert(
+        "Hello, there!",
+        isPresented: $alertIsVisible,
+        actions: {
+          Button("Awesome") {
+            print("Alert closed")
+          }
+        },
+        message: {
+          var roundedValue: Int = Int(sliderValue.rounded())
+          Text("""
+            The slider's value is \(roundedValue).
+            You scored \(game.points(sliderValue: roundedValue)) points this round.
+          """)
+        }
+      )
+    }
+  }
+}
+
+#Preview {
+  ContentView()
+}
