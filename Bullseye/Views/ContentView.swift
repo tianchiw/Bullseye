6883f72c9c0725986ea2f542525864f19ad10bbe
Tianchi "Maverick" Wu <tianchiwu.27@gmail.com>
2025-08-22 13:22:22 -0400

    Refine code & Add score calculate

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -1,9 +1,9 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var alertIsVisible: Bool = false
-  @State private var sliderValue: Double = 50.0
-  @State private var game: Game = Game()
+  @State private var alertIsVisible = false
+  @State private var sliderValue = 50.0
+  @State private var game = Game()
   
   var body: some View {
     VStack {
@@ -36,7 +36,7 @@ struct ContentView: View {
           }
         },
         message: {
-          var roundedValue: Int = Int(sliderValue.rounded())
+          let roundedValue = Int(sliderValue.rounded())
           Text("""
             The slider's value is \(roundedValue).
             You scored \(game.points(sliderValue: roundedValue)) points this round.
