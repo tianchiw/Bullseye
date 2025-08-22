2256596ae5656b776d4b63ef90ed9d04dc8330e4
Tianchi "Maverick" Wu <tianchiwu.27@gmail.com>
2025-08-22 13:40:33 -0400

    Styling the UI

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -7,12 +7,13 @@ struct ContentView: View {
   
   var body: some View {
     VStack {
-      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+      Text("🎯🎯🎯\nPut the Bullseye as close as you can to".uppercased())
         .bold()
         .multilineTextAlignment(.center)
         .lineSpacing(4.0)
         .font(.footnote)
         .kerning(2.0)
+        .padding(.horizontal, 30)
       Text(String(game.target))
         .fontWeight(.black)
         .font(.largeTitle)
@@ -24,9 +25,18 @@ struct ContentView: View {
         Text("100")
           .bold()
       }
-      Button("Hit me") {
+      .padding()
+      Button("Hit me".uppercased()) {
         alertIsVisible = true
       }
+      .padding(20.0)
+      .background(
+        Color.blue
+      )
+      .foregroundColor(Color.white)
+      .cornerRadius(21.0)
+      .bold()
+      .font(.title3)
       .alert(
         "Hello, there!",
         isPresented: $alertIsVisible,
